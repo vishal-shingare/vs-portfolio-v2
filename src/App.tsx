ae0c88ec3d3c329fc2419d3f8899b09b18528ca0
Vishal Shingare <visshi7p@splm.siemens.com>
2022-04-30 22:54:38 +0530

    feat : created and init config for vs-portfolio site.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-2xl font-bold text-indigo-500">
+        React and Tailwind with Vitejs! By Vishal
+      </h1>
+    </div>
+  );
+}
+
+export default App;
