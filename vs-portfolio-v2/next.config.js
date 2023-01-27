2c7375acc6b6599863137c3c66b9b9c76763dc10
vishal-shingare <vishalshingare29@gmail.com>
2023-01-27 22:58:29 +0530

    VS-105/added hero component.

--- vs-portfolio-v2/next.config.js
+++ vs-portfolio-v2/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mdbcdn.b-cdn.net",
+        port: "",
+        pathname: "/img/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- vs-portfolio-v2/pages/index.tsx
+++ vs-portfolio-v2/pages/index.tsx
@@ -1,28 +1,34 @@
-import Head from 'next/head'
-import { Inter } from '@next/font/google'
-import Header from '@/components/Header'
+import { NextPage } from "next";
+import Head from "next/head";
+import { Inter } from "@next/font/google";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
-    <>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Vishal Shingare Portfolio</title>
       </Head>
 
-      {/* { Header } */}
-      <Header  />
-      
+      <Header />
+
       {/* { Hero } */}
-      
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+
       {/* { About } */}
 
       {/* { Experince } */}
-      
+
       {/* { Projects } */}
 
       {/* { Contact Me } */}
-    </>
-  )
-}
+    </div>
+  );
+};
+
+export default Home;
